fd8848963d71b72b12422aa30ed110134911ab20
Rodrigo Melo <rodrigo.melo@skipthedishes.ca>
2020-01-18 18:37:58 -0600

    Included query to get client by ID

--- index.js
+++ index.js
@@ -2,11 +2,22 @@ import { GraphQLServer } from 'graphql-yoga';
 import connection from './infrastructure/mysql-connection';
 import clientDAO from './model/client-dao';
 
+// TODO See how to return a custom error with GraphQL.
+// TODO Include Authentication/Authorization
+// TODO Separate resolvers and queries in specific files
+// TODO See how to implement 12 Factor App methodology with GraphQL/Nodejs (Circuit braker, retry, fallback, etc.)
+// TODO Include Typescript to the project
+
 const resolvers = {
   Query: {
     clients: () =>
       clientDAO
         .findAll()
+        .then(result => result)
+        .catch(error => error),
+    client: (root, { id }) =>
+      clientDAO
+        .findById(id)
         .then(result => result)
         .catch(error => error)
   },
